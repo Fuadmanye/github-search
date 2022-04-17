1adde1ecd7d724ab43f3984ea63904bc93c123f7
fuad manye / <fahadbasi333@gmail.com>
2022-04-18 01:32:36 +0300

    create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RepoResultComponent } from './repo-result/repo-result.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { NameResultComponent } from './name-result/name-result.component';
+import { NameRepoComponent } from './name-repo/name-repo.component';
+import { MyHomeComponent } from './my-home/my-home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RepoResultComponent,
+    PageNotFoundComponent,
+    NavBarComponent,
+    NameResultComponent,
+    NameRepoComponent,
+    MyHomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/name-repo/name-repo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-name-repo',
+  templateUrl: './name-repo.component.html',
+  styleUrls: ['./name-repo.component.css']
+})
+export class NameRepoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
